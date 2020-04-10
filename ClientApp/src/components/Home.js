3177824ca74225f1cd15258c639ad84aba2dd6e8
Jared Farkas <jaredmfarkas@gmail.com>
2020-04-10 08:39:49 -0700

    Finish creating powerpoint object to store image, text, and titles from user input

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import "./styles.css";
+import axios from 'axios';
+import qs from 'qs';
 
 export class Home extends Component {
   static displayName = Home.name;
 
   constructor (props) {
     super(props);
-    this.state = { images: [], title: ""};
+    this.state = { images: [], title: "", text: ""};
     this.getImages = this.getImages.bind(this);
+    this.createPpt = this.createPpt.bind(this);
+    this.selectImage = this.selectImage.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
   }
 
 handleChange(event){
@@ -15,7 +21,21 @@ handleChange(event){
   console.log(this.state);
 }
 
-getImages(words){
+handleTextChange(event){
+  this.setState({text: event.target.value})
+  console.log(this.state);
+}
+
+getImages(){
+  console.log('images');
+  let bolded = this.state.text.split(" ");
+  bolded = bolded.map(e=>{
+    if(e.split("(b)").length > 0){
+      return e.split("(b)")[1];
+    }
+  })
+  let words = this.state.title + bolded.join(" ");
+  console.log(words);
   fetch('api/SampleData/ImageList/'+words)
     .then(response => response.json())
     .then(data => {
@@ -24,10 +44,67 @@ getImages(words){
     });
 }
 
+createPpt(){
+  console.log('clicked');
+  let bolded = this.state.text.split(" ");
+  bolded = bolded.map(e=>{
+    if(e.split("(b)").length > 1){
+      return e.split("(b)")[1];
+    }
+    else {
+      return e;
+    }
+  })
+  let words = bolded.join(" ");
+  let selected = this.state.images.filter(e=>
+  (e.selected === true))
+  let jason = qs.stringify({
+        "title": this.state.title,
+        "text": words,
+        "imageList": selected
+    })
+    console.log(jason);
+    axios({
+      method: 'post',
+      url: 'api/sampleData/CreatePpt/',
+      data:{
+        jason,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) =>
+  {
+    console.log(response);
+  },
+(error) => {
+  console.log(error)
+});
+  // fetch('api/sampleData/CreatePpt'),{
+  //   method: 'GET',
+  //   headers:{
+  //     'Content-Type': 'application/json'
+  //   },
+  //   credentials: "same-origin",
+  //   body: jason
+  //   }
+}
+
+selectImage(uri){
+  for(let i = 0; i< this.state.images.length; i++){
+    if(this.state.images[i].imageURI === uri){
+      this.state.images[i].selected = !this.state.images[i].selected;
+    }
+    this.setState({images: this.state.images});
+  }
+console.log(this.state);
+}
+
 
   render () {
     return (
       <div>
+        <button className='link' onClick={()=>this.createPpt()}> Create Powerpoint</button>
         <form>
           <label>Title:
             <input type = 'text' id ='title' name = 'title' value = {this.state.title} onChange={this.handleChange}/>
@@ -35,14 +112,17 @@ getImages(words){
           <br></br>
           <label>Text: (Use (b) before a word to bold it)
             <textarea type = 'textarea' rows = '5'
-              cols = '65' id ='text' name = 'text'/>
+              cols = '65' id ='text' name = 'text' value = {this.state.text} onChange={this.handleTextChange}/>
           </label>
-          <h3 onClick={()=>this.getImages(this.state.title)}>Submit</h3>
+          <h3 className = 'link' onClick={()=>this.getImages()}>Find Images</h3>
+
         </form>
           {this.state.images.map( (e,index)=>
-                <img  id = {index} src = {e.imageURI} onClick={(index, e.imageURI)=>this.selectImage}/>
+                  <img className ={e.selected ? 'highlight' : 'dont'} id = {index} src = {e.imageURI} onClick={()=>this.selectImage(e.imageURI)}/>
           )}
+
       </div>
+
     );
   }
 }
--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.Office.Interop.Powerpoint;
 using Microsoft.AspNetCore.Mvc;
 
 namespace seh.Controllers
@@ -20,13 +22,74 @@ namespace seh.Controllers
          return Images.GetImageList(rawData);
         }
 
+        [HttpPost("[action]/")]
+        public void CreatePpt(String data)
+        {
+          // Console.WriteLine(data);
+          using (var reader = new StreamReader(Request.Body))
+          {
+            PptSlide ppt = new PptSlide();
+            var body = reader.ReadToEnd();
+            Console.WriteLine(body);
+            String splitTheBody = body;
+            ppt.title=splitTheBody.Split('=')[1].Split('&')[0];
+            ppt.text=splitTheBody.Split('=')[2].Split('&')[0];
+            ppt.imageList = PptSlide.getImages(splitTheBody);
+            //Console.WriteLine(splitTheBody.Split('=')[1]);
+            //PptSlide ppt = JsonConvert.DeserializeObject<PptSlide>(body);
+            Console.WriteLine("AHHHHHHH");
+            Console.WriteLine(ppt.title);
+            Console.WriteLine(ppt.text);
+          }
+
+        }
+
+        public class PptSlide
+        {
+          public string title { get; set; }
+          public string text { get; set; }
+          public List<Images> imageList { get; set; }
+
+          public PptSlide(){
+            imageList = new List<Images>();
+            title = "";
+            text = "";
+          }
+
+          public static List<Images> getImages(String html)
+          {
+
+            List<Images> images = new List<Images>();
+              int i = html.IndexOf("imageList", StringComparison.Ordinal);
+              i = html.IndexOf("imageURI", i, StringComparison.Ordinal);
+             Console.WriteLine(html);
+             int loops = 0;
+            while( i >= 0 && loops < 20)
+            {
+              i = html.IndexOf("=", i, StringComparison.Ordinal);
+              i+=5;
+              int j = html.IndexOf("&",i,StringComparison.Ordinal);
+              string uri = html.Substring(i, j - i);
+              Console.WriteLine(uri);
+              images.Add(new Images(uri));
+              i = html.IndexOf("imageURI", i, StringComparison.Ordinal);
+              loops++;
+
+            }
+            return images;
+          }
+
+        }
+
         public class Images
         {
-
             public string ImageURI { get; set; }
+            public bool Selected { get; set; }
+
             public Images(string URI)
             {
               ImageURI = URI;
+              Selected = false;
             }
 
           public static string GetImages(string Words)
